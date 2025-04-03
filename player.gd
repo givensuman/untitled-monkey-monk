3737f578e49dfd8212369e728f71f68105522706
seamusrooneyy <seamusrooneyy@gmail.com>
2025-04-03 09:49:36 -0400

    Class 4/3

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+var can_dblJump = false
 var button = "res://button.tscn"
 
 
@@ -11,10 +12,18 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 1.5
+	if is_on_floor(): 
+		can_dblJump = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		can_dblJump = true
+		# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and can_dblJump:
+		velocity.y = JUMP_VELOCITY
+		can_dblJump = false
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
