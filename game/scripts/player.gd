616ba3b6f71dde5a8576cecf646f2702508957f1
cameronbaum0124 <cameronbaum0124@gmail.com>
2025-04-11 14:51:37 -0400

    added animation player to Monkey, also added walk right and walk left and right idle.
    Could not figure out in the little time I had how to make it stop at pointing left when you had last moved left and also how to get the jump animation to play.

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -14,10 +14,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta * 1.5
 	if is_on_floor(): 
 		can_dblJump = false
-
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		if direction:
+			if velocity.x > 0 and velocity.y == 0:
+				%AnimationPlayer.play("jump_right")
+			elif velocity.x < 0 and velocity.y < 0: 
+				$AnimationPlayer.play("jump_left")
 		can_dblJump = true
 		# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and can_dblJump:
@@ -27,11 +34,17 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.x > 0:
+			$AnimationPlayer.play("walk_right")
+		elif velocity.x < 0:
+			$AnimationPlayer.play("walk_left")
 	else:
+		%AnimationPlayer.play("idle_right")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	
 	
 	move_and_slide()
