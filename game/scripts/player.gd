95b4a5f9484918d4c84f4b7b0b89d856dfde7ad3
given <givensuman@gmail.com>
2025-04-11 23:42:38 -0400

    re-do sounds

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+const RANDOM_VOLUME_AMOUNT = 5
+const RANDOM_VOLUME_TIMEOUT = 0.1
+
 var can_dblJump = false
 var button = "res://button.tscn"
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,15 +22,17 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$JumpSound.play()
 		if direction:
 			if velocity.x > 0 and velocity.y == 0:
 				%AnimationPlayer.play("jump_right")
 			elif velocity.x < 0 and velocity.y < 0: 
 				$AnimationPlayer.play("jump_left")
 		can_dblJump = true
-		# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and can_dblJump:
 		velocity.y = JUMP_VELOCITY
+		$JumpSound.play()
 		can_dblJump = false
 		
 
@@ -37,6 +41,14 @@ func _physics_process(delta: float) -> void:
 	# var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		if is_on_floor() and $WalkSound/Timer.is_stopped():
+			$WalkSound/Sound.volume_db = -5 + (RANDOM_VOLUME_AMOUNT * 2 * randf()) - RANDOM_VOLUME_AMOUNT
+			$WalkSound/Timer.wait_time = 0.5 + (RANDOM_VOLUME_TIMEOUT * 2 * randf()) - RANDOM_VOLUME_TIMEOUT
+			
+			$WalkSound/Sound.play()
+			$WalkSound/Timer.start()
+		
 		if velocity.x > 0:
 			$AnimationPlayer.play("walk_right")
 		elif velocity.x < 0:
