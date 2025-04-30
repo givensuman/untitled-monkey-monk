304200343c0e499d03257e1936050924b2afc29b
given <givensuman@gmail.com>
2025-04-29 22:21:35 -0400

    Add checkpoints animation and logic

--- game/scripts/checkpoint.gd
+++ game/scripts/checkpoint.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal checkpoint_activated
 
 @onready var checkpoint_sound = $CheckpointSound
+static var active_checkpoint: Area2D = null
 
 func _ready():
 	# Connect both area and body signals
@@ -15,15 +16,29 @@ func _ready():
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if body.update_checkpoint(self):
-			checkpoint_sound.play()
-		emit_signal("checkpoint_activated")
+		activate_checkpoint(body)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		# Get the player node (parent of the area)
 		var player = area.get_parent()
 		if player and player.is_in_group("player"):
-			if player.update_checkpoint(self):
-				checkpoint_sound.play()
-			emit_signal("checkpoint_activated")
+			activate_checkpoint(player)
+
+func activate_checkpoint(player: Node2D) -> void:
+	if player.update_checkpoint(self):
+		# Deactivate previous checkpoint if it exists
+		if active_checkpoint != null and active_checkpoint != self:
+			active_checkpoint.deactivate()
+		
+		# Set this as the active checkpoint
+		active_checkpoint = self
+		checkpoint_sound.play()
+		$AnimationPlayer.current_animation = "incense"
+		$AnimationPlayer.play()
+		emit_signal("checkpoint_activated")
+
+func deactivate() -> void:
+	$AnimationPlayer.stop()
+	$AnimationPlayer.current_animation = "RESET"
+	$AnimationPlayer.play()
