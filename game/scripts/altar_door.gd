0c77374da5b8fd72f4c0be13a2ce8d261fb8f0d7
given <givensuman@gmail.com>
2025-04-29 23:33:04 -0400

    add bananas, door wont open tho

---
+++ game/scripts/altar_door.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var animation_player = $AnimationPlayer
+
+func _on_all_bananas_collected() -> void:
+	# Play door opening animation
+	print("All bananas collected!")
+	#animation_player.play("Open")
+	$Sprite2D.frame += 5
---
+++ game/scripts/banana_manager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+signal all_bananas_collected
+
+var blue_banana_collected := false
+var green_banana_collected := false
+var purple_banana_collected := false
+
+func collect_banana(color: String) -> void:
+	match color:
+		"blue":
+			blue_banana_collected = true
+		"green":
+			green_banana_collected = true
+		"purple":
+			purple_banana_collected = true
+	
+	check_all_collected()
+
+func check_all_collected() -> void:
+	if blue_banana_collected and green_banana_collected and purple_banana_collected:
+		print("All bananas collected, emitting signal!")
+		emit_signal("all_bananas_collected")
+
+func _ready() -> void:
+	# Connect to the door to open it when all bananas are collected
+	var door = get_tree().get_root().get_node_or_null("World/Altar_Door")
+	if door:
+		connect("all_bananas_collected", Callable(door, "_on_all_bananas_collected"))
+		print("Connected to door successfully")
+	else:
+		print("Door not found at path World/Altar_Door")
