a6d995d405cf854c5079270fbcb52da2a3b98462
cameronbaum0124 <cameronbaum0124@gmail.com>
2025-04-30 19:50:37 -0400

    FINISHED GAME

--- game/scenes/gibbon.gd
+++ game/scenes/gibbon.gd
@@ -3,3 +3,14 @@ extends Node2D
 func _ready() -> void:
 	$AnimationPlayer.play("golden_banana")
 	print($Marker2D.global_position)
+	$Area2D.body_entered.connect(_on_area_2d_body_entered)
+
+
+
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("character entered")
+		get_tree().quit()
--- game/scripts/title_screen.gd
+++ game/scripts/title_screen.gd
@@ -4,18 +4,37 @@ var tutorial_music = preload("res://sounds/music/tutorial_drums.mp3")
 @onready var music_player = $MusicPlayer
 
 func _ready():
-	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
-	$VBoxContainer/CreditsButton.pressed.connect(_on_credits_pressed)
-	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
-	
+	$ColorRect/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	$ColorRect/VBoxContainer/CreditsButton.pressed.connect(_on_credits_pressed)
+	$ColorRect/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+	$AnimationRect/AnimationPlayer.animation_finished.connect(_on_cutscene_finished)
+	$AnimationRect.hide()
 	# Start playing the tutorial music
 	music_player.stream = tutorial_music
 	music_player.play()
 	music_player.process_mode = Node.PROCESS_MODE_ALWAYS # Keep playing during scene changes
 
-func _on_play_pressed():
+'''func _on_play_pressed():
 	# Change to the main game scene without stopping music
-	get_tree().change_scene_to_file("res://game/scenes/world.tscn")
+	$Wallpaper1.queue_free()
+	$Wallpaper2.queue_free()
+	$ColorRect.hide()
+	$AnimationRect.show()
+	$AnimationRect/AnimationPlayer.play("cutscene")
+	if $AnimationRect/AnimationPlayer.animation_finished():
+		get_tree().change_scene_to_file("res://game/scenes/world.tscn")'''
+		
+func _on_play_pressed():
+	$Wallpaper1.queue_free()
+	$Wallpaper2.queue_free()
+	$ColorRect.hide()
+	$AnimationRect.show()
+	$AnimationRect/AnimationPlayer.play("cutscene")
+
+func _on_cutscene_finished(anim_name):
+	if anim_name == "cutscene":
+		get_tree().change_scene_to_file("res://game/scenes/world.tscn")
+
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://game/scenes/credits_screen.tscn")
